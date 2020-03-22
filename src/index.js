3782f3d42a9984271555f7128ec7b6b681d8761b
최예지 <yeji@choeyejiui-MacBookPro.local>
2020-03-22 15:34:30 +0900

    충돌해결

--- src/index.js
+++ src/index.js
@@ -9,19 +9,9 @@ import { createStore } from "redux";
 import { rootReducer } from "store/rootReducer";
 import "styles/reset.css";
 
-<<<<<<< HEAD
 ReactDOM.render(
   <Provider store={createStore(rootReducer)}>
     <Routes />
   </Provider>,
   document.getElementById("root")
 );
-=======
-
-ReactDOM.render(
-<Provider store={createStore(rootReducer)}>
-    <Routes />  
-</Provider>, 
-document.getElementById("root")
-);
->>>>>>> a69a044ce24f0fb501be996c38d29c16e3d97e50
